6dc60efd49f948013b8a09b336f9c14293c2769d
shivanitayde <shivanitayde@608>
2025-09-18 23:07:07 +0530

    adding OnBoarding pages

---
+++ pregmaa/lib/Theme/ColorsPallet/Colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class Colorspalette {
+  static const white = Colors.white;
+  static const textColor = Colors.black;
+  static const indexColor = Color(0xFFEC407A);
+}
